b4edc1ec166b369af303a360ae096e79b010acb5
BELCHONOK <karantov@bk.ru>
2024-04-07 23:24:35 +0300

    Готовый проект

---
+++ functions.py
@@ -0,0 +1,103 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+"""
+Этот файл содержит все функции, которые будут испольльзоваться в проекте
+"""
+
+@st.cache_data
+def load_html_data(url, num_table= 0):
+    """
+    Функция, которая возвращает датафрейм, хранящийся по HTML адресу
+
+    Parameters:
+        url - html adress
+        num_table - number of table keeping in web_page
+
+    Return:
+        pd.DataFrame
+    """
+    df = pd.read_html(url)
+    return pd.DataFrame(df[num_table])
+
+
+#@st.cache_data
+def load_excel_data(path):
+    """
+    Загрузка данных из Excel файла с выбором всех страниц в документе
+
+    """
+    return pd.read_excel(path, sheet_name=None, skiprows=2)# выбрали все страницы в документе
+    
+    
+def choose_areas_and_concat(df_1, df_2, areas):
+    df_1 = df_1[df_1['Area'].isin(areas)].copy().reset_index(drop = True)
+    df_2 = df_2[df_2['Area'].isin(areas)].copy().reset_index(drop = True)
+
+    return pd.concat([df_1, df_2.drop(columns=['Area'])], axis=1)
+
+
+def real_wage(df,years,area,inflation):
+    nominal_wage = df[df['Area']==area]
+    real_wage = []
+    for year in years:
+        salary = nominal_wage[year].values[0]
+        prev_infl = inflation[inflation['Год']==year-1].values[0][1]
+        recalc_salary = salary/(1+(prev_infl*0.01))
+        real_wage.append(recalc_salary)
+    return real_wage
+
+
+def plot_xy(values, years, title, ylabel, xlabel):
+    """
+    Функция строит линейный график зависимости с сеткой
+
+    """
+    plt.figure(figsize=(10,5))
+    sns.lineplot(x= years, y= values, color= '#FACFCE')
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.xticks(years,rotation = 'vertical')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+def bar_plot(years, values, title, ylabel, xlabel):
+    """
+    Функция строит столбчатую диаграмму, с надписями значений над столбцами
+
+    """
+    plt.figure(figsize=(15, 7))
+    sns.barplot(x=years, y=values, color= '#99C274')
+    # Добавление значений над столбцами
+    for i, value in enumerate(values):
+        plt.text(i, value, f'{value:.2f}%', ha='center', va='bottom')
+
+    # Дополнительные настройки
+    plt.grid(axis= 'y')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    plt.show()
+
+def plot_2graphs(years, values_1, values_2, title, xlabel, ylabel, label_1, label_2):
+    plt.figure(figsize=(10,5))
+    sns.lineplot(x= years, y= values_1, label= label_1, color= '#40E0D0', marker= 'o')
+    sns.lineplot(x= years, y= values_2, label= label_2, color='#FFC0CB', marker= 'o')
+    plt.title(title)
+    plt.legend()
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.xticks(years, rotation= 'vertical')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+
---
+++ pages/2_GDP_page.py
@@ -0,0 +1,87 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st 
+from functions import *
+import numpy as np
+st.set_option('deprecation.showPyplotGlobalUse', False)
+sns.set_style({'axes.facecolor':'23606E',
+               'figure.facecolor': '23606E', 
+               'axes.labelcolor': 'F5F5F5',
+               'axes.axisbelow': True,
+               'axes.edgecolor': '327E8F',
+               'text.color':'F5F5F5',
+               'xtick.color': 'F5F5F5',
+              'ytick.color': 'F5F5F5',
+              'grid.color': '327E8F'})
+#sns.set_facecolor("#23606E")
+
+
+st.set_page_config(page_title="Анализ ВВП", page_icon="🤡")
+
+GDPs = load_excel_data('data/Rosstat National Accounts 1995.xlsx')
+years = np.arange(2000, 2024, 1)
+
+GDP = pd.DataFrame(GDPs['1'])
+GDP = pd.concat([GDP, pd.DataFrame(GDPs['2']).drop(columns=2011)], axis=1)
+GDP = GDP.iloc[:, 5:]
+
+deflation = pd.DataFrame(GDPs['9'])
+deflation = pd.concat([deflation, pd.DataFrame(GDPs['10'])], axis=1)
+deflation = deflation.iloc[:,4:]
+
+real_GDP = GDP*100/deflation
+
+
+st.title("Анализ ВВП в России в период с 2000 по 2023 годы")
+st.text("Представляю анализ ВВП России с учетом дефлятора")
+
+#from PIL import Image
+#image = Image.open("data/money.jpeg")
+#st.image(image)
+
+st.sidebar.title('About')
+
+radio = st.sidebar.radio('Вывести график', 
+    options=['Номинальный ВВП',
+             'Реальный ВВП',
+             'Реальный VS Номинальный ВВП', 
+             'Разность между рельным и номинальным ВВП'])
+
+if radio == 'Номинальный ВВП':
+    values = GDP.values.flatten()
+    title = 'Номинальный ВВП в период с 2000 по 2023 год'
+    ylabel = 'млрд. рублей'
+    fig = plot_xy(values, years, title, ylabel=ylabel, xlabel= None )
+    st.pyplot(fig)
+    st.info('Можно наблюдать рост номинального ВВП, но это мало о чем говорит')
+elif radio == 'Реальный ВВП':
+    values = real_GDP.values.flatten()
+    title = 'Реальный ВВП в период с 2000 по 2023 год'
+    ylabel = 'млрд. рублей'
+    fig = plot_xy(values, years, title, ylabel=ylabel, xlabel= None )
+    st.pyplot(fig)
+    st.info('Реальный ВВП тоже растет, что говорит об улучшении экономики России') 
+elif radio == 'Реальный VS Номинальный ВВП':
+    title = 'Сравнение реального и номинального ВВП'
+    ylabel =  'млрд. рублей'
+    label_1 = 'Номинальный ВВП'
+    label_2 = 'Реальный ВВП'
+    fig = plot_2graphs(years, 
+                 GDP.values.flatten(), 
+                 real_GDP.values.flatten(),
+                 title,
+                 xlabel= None, 
+                 ylabel= ylabel, 
+                 label_1 = label_1,
+                 label_2= label_2)
+    st.pyplot(fig)
+    st.info('Сравнивая два графика, мы видим лишь то, что дефляция все-таки есть (куда без нее)')
+
+elif radio == 'Разность между рельным и номинальным ВВП':
+    values = ((1 - real_GDP.values.flatten() / GDP.values.flatten()) * 100)
+    title= 'Разница между номинальным и реальным ВВП'
+    fig = bar_plot(years, values, title, ylabel= '%', xlabel=None)
+    st.pyplot(fig)
+    st.info('Разница довольно сильная, сложно спрогнозировать будущее')
+    
